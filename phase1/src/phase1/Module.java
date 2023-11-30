4813e766f70a6db3c2036ae80356834869050ae2
sanaaazza <azzasanaa02@gmail.com>
2023-11-30 14:30:54 +0000

    phase1

--- phase1/src/phase1/Module.java
+++ phase1/src/phase1/Module.java
@@ -1,4 +1,44 @@
 package phase1;
 
-public class Module {
+public class Module {private String intitulé;
+    private Filière filière;
+    private Enseignant professeur;
+    public Module(String intitulé, Filière filière, Enseignant professeur){
+        this.intitulé=intitulé;
+        this.filière=filière;
+        this.professeur=professeur;
+    }
+
+    public String getIntitulé() {
+        return intitulé;
+    }
+
+    public Filière getFilière() {
+        return filière;
+    }
+
+    @Override
+    public String toString() {
+        return "Module{" +
+                "intitulé='" + intitulé + '\'' +
+                ", filière=" + filière +
+                ", professeur=" + professeur +
+                '}';
+    }
+
+    public Enseignant getProfesseur() {
+        return professeur;
+    }
+
+    public void setIntitulé(String intitulé) {
+        this.intitulé = intitulé;
+    }
+
+    public void setFilière(Filière filière) {
+        this.filière = filière;
+    }
+
+    public void setProfesseur(Enseignant professeur) {
+        this.professeur = professeur;
+    }
 }
