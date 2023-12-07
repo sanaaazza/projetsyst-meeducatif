b3f1f476a89b126859dae4ee2e58e0f9dc3bbc9b
sanaaazza <azzasanaa02@gmail.com>
2023-12-07 13:45:31 +0000

    phase1

--- phase1/src/Services/Database.java
+++ phase1/src/Services/Database.java
@@ -7,7 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Database {
-  public static   List<Etudiant> etudiants=new ArrayList<Etudiant>();
+    public static int Id_ET=0;
+    public static int Id_ES=0;
+    public static int Id_MD=0;
+    public static int Id_FL=0;
+    public static int Id_DP=0;
+    public static   List<Etudiant> etudiants=new ArrayList<Etudiant>();
     public static   List<Enseignant> enseignant = new ArrayList<Enseignant>();
     public static   List<Filiere> filieres = new ArrayList<Filiere>();
     public static   List<Module> modules = new ArrayList<Module>();
