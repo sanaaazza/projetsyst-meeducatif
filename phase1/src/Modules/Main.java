6cb95af932eb4a4c7c44ded2a4cc24073e80d685
sanaaazza <azzasanaa02@gmail.com>
2023-12-07 13:14:56 +0000

    phase1

--- phase1/src/Modules/Main.java
+++ phase1/src/Modules/Main.java
@@ -1,8 +1,18 @@
 package Modules;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public  static void main(String[] args){
-        List<String> liste = new ArrayList<>();
+
+        List<Etudiant> etudiants = new ArrayList();
+        List<Enseignant> enseignant = new ArrayList();
+        List<Filiere> filieres = new ArrayList();
+        List<Module> modules = new ArrayList();
+        List<Departement> departement = new ArrayList();
+
+
+
     }
 }
