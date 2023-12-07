005292290c59e9cd2bbae5c3dbc58fe83de35fa5
sanaaazza <azzasanaa02@gmail.com>
2023-12-07 13:36:57 +0000

    phase1

--- phase1/src/Modules/Main.java
+++ phase1/src/Modules/Main.java
@@ -6,11 +6,8 @@ import java.util.Scanner;
 public class Main {
     public  static void main(String[] args){
 
-        List<Etudiant> etudiants = new ArrayList();
-        List<Enseignant> enseignant = new ArrayList();
-        List<Filiere> filieres = new ArrayList();
-        List<Module> modules = new ArrayList();
-        List<Departement> departement = new ArrayList();
+
+
 
 
 
---
+++ phase1/src/Services/Database.java
@@ -0,0 +1,15 @@
+package Services;
+
+import Modules.*;
+import Modules.Module;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+  public static   List<Etudiant> etudiants=new ArrayList<Etudiant>();
+    public static   List<Enseignant> enseignant = new ArrayList<Enseignant>();
+    public static   List<Filiere> filieres = new ArrayList<Filiere>();
+    public static   List<Module> modules = new ArrayList<Module>();
+    public static  List<Departement> departement = new ArrayList<Departement>();
+}
